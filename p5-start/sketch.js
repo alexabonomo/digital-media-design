a8410bd2329027b95dd8848cfaabafba95a7dde8
alexa <me@alexaann.net>
2023-03-29 00:02:09 -0700

    exercise 2

--- p5-start/sketch.js
+++ p5-start/sketch.js
@@ -1,7 +1,10 @@
+let mood;
+
 function setup() {
     // make the div called "mySketch" a p5.js canvas element
     let cnv = createCanvas(windowWidth, windowHeight);
     cnv.parent('mySketch');
+
 
   }
   
@@ -9,6 +12,17 @@ function setup() {
     //draw a white circle in the middle of the page
     fill(255);
     ellipse(width/2, height/2, 100,100);
+    background(0)
+
+    // console.log( "x: " + mouseX + "," + "y: " + mouseY)
+
+    if(mouseX < width/2){
+      mood = "dark";
+    } else {
+      mood = "light";
+    }
+
+    console.log(mood);
 
   }
   
